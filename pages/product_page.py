61ba5fc3a3dbd08c9785f4ac5d34319c1610d5c9
Flevity <flevity@yandex.ru>
2022-05-03 21:13:05 +0300

    Pageobject for product page added

---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_card(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_CARD).click()
+
+    def should_be_correct_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        notification_product_name = self.browser.find_element(*ProductPageLocators.NOTIFICATION_PRODUCT_NAME).text
+        assert product_name == notification_product_name, "The product name in the notification " \
+                                                          "doesn't match product name"
+
+    def should_be_correct_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        notification_price = self.browser.find_element(*ProductPageLocators.NOTIFICATION_PRICE).text
+        assert product_price == notification_price, "The price in the notification doesn't match product price"
