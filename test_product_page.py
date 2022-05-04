310d1533296e610ec15e9e88ca826e88c1b18e86
Flevity <flevity@yandex.ru>
2022-05-05 00:31:00 +0300

    renamed parametrized test, added test without parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -5,7 +5,18 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
+# параметризация из теста для ревью была убрана по совету модератора, если очень хочется - это следующий тест в файле
 @pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_card()
+    page.solve_quiz_and_get_code()
+    page.should_be_correct_product_name()
+    page.should_be_correct_price()
+
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -17,7 +28,7 @@ from .pages.basket_page import BasketPage
                                                "/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket_parametrized(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_card()
