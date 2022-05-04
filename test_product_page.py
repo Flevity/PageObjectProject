f8f248d22d55e4364b91309c211c4b123b12b6cc
Flevity <flevity@yandex.ru>
2022-05-04 20:52:59 +0300

    Added tests for authorized users

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 import pytest
+import time
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
@@ -71,3 +72,32 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
+
+
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        email = str(time.time()) + "@fakemail.org"
+        # using password same as email
+        login_page.register_new_user(email, email)
+        login_page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_card()
+        page.solve_quiz_and_get_code()
+        page.should_be_correct_product_name()
+        page.should_be_correct_price()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
